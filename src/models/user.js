8cf1f33f83123223556b067bb68ecedec7029006
Jessica Simon <jsimon2789@gmail.com>
2020-04-06 12:48:20 -0400

    Create initial user endpoint

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid.')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot include "password"')
+      }
+    }
+  }
+})
+
+module.exports = User
+
+
